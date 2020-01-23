373756c5fa14b2bb504ed524ced3d782dbbdd8da
Alexander Peters <businessonly1011@gmail.com>
2020-01-23 00:35:56 -0500

    Minor issues fixed, and build test complete.

--- src/main/java/frc/robot/commands/DesiredPose_SMDB_Sender.java
+++ src/main/java/frc/robot/commands/DesiredPose_SMDB_Sender.java
@@ -2,14 +2,18 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
-
-import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj.trajectory.Trajectory.State;
 
 public class DesiredPose_SMDB_Sender extends CommandBase {
   private final Timer m_timer = new Timer();
   private final Trajectory m_trajectory;
   private State currentTrajectoryState;
+  private Pose2d currentlyDesiredPose;
+  private double currentlyDesiredVelocity;
+  private double currentlyDesiredAngle;
 
   public DesiredPose_SMDB_Sender(Trajectory trajectory) {
     m_trajectory = trajectory;
@@ -23,13 +27,14 @@ public class DesiredPose_SMDB_Sender extends CommandBase {
 
   @Override
   public void execute() {
-    currentTrajectoryState = m_trajectory.sample(curTime);
+    double currentTime = m_timer.get();
+    currentTrajectoryState = m_trajectory.sample(currentTime);
     currentlyDesiredPose = currentTrajectoryState.poseMeters;
     currentlyDesiredVelocity = currentTrajectoryState.velocityMetersPerSecond;
     currentlyDesiredAngle = currentlyDesiredPose.getRotation().getDegrees();
     
     SmartDashboard.putNumber("Desired Velocity (m/s): ", currentlyDesiredVelocity);
-    SmartDashboard.putNumber("Desired Heading (deg): ", currentlyDesiredAngle)
+    SmartDashboard.putNumber("Desired Heading (deg): ", currentlyDesiredAngle);
   }
 
   @Override
