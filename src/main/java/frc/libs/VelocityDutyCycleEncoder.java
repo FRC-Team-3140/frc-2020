7fbe800b8b81b3e09c680bf479e17aa1a6cb9da9
Alexander Peters <businessonly1011@gmail.com>
2020-01-26 20:17:59 -0500

    Looped Encoder Done

--- src/main/java/frc/libs/VelocityDutyCycleEncoder.java
+++ src/main/java/frc/libs/VelocityDutyCycleEncoder.java
@@ -2,45 +2,62 @@ package frc.libs;
 
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Timer;
+import frc.loopController.Loop;
+import frc.loopController.Looper;
 
-public class VelocityDutyCycleEncoder extends DutyCycleEncoder {
+public class VelocityDutyCycleEncoder extends DutyCycleEncoder implements Loop {
   private final Timer m_timer = new Timer();
+  private Looper looper = new Looper(0.01);
   private double lastPosition;
   private double lastTime;
+  private double currentVelocity;
   
   public VelocityDutyCycleEncoder(int channel) {
     super(channel);
     m_timer.reset();
     m_timer.start();
+
     lastPosition = getDistance();
     lastTime = m_timer.get();
+
+    looper.register(this);
+    looper.start();
   }
   
   @Override
   public void reset() {
     super.reset();
     m_timer.reset();
+    
     lastPosition = getDistance();
     lastTime = m_timer.get();
   }
   
   // Returns Encoder Distance / Seconds
   public double getRate() {
-    // Get current Position and Time
+    return currentVelocity;
+  }
 
-    // ?? Do I need to put a deadband on the encoder distance so velocity doesn't drift when the bot
-    // isn't moving i.e. if distance < 0.001 set to 0????
+  @Override
+  public void onStart() {
+  }
+
+  @Override
+  public void onLoop() {
+    // Get current Position and Time
     double currentPosition = getDistance();
     double currentTime = m_timer.get();
     
     // Calculate current Velocity
-    double currentVelocity = (currentPosition - lastPosition)
+    currentVelocity = (currentPosition - lastPosition)
       / (currentTime - lastTime);
     
     // Set previous Position and Time to current Position and Time
     lastPosition = currentPosition;
     lastTime = currentTime;
-    
-    return currentVelocity;
+  }
+
+  @Override
+  public void onStop() {
   }
 }
