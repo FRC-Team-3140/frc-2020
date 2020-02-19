71991cc6aae280930b562501fdcdc229b1c9aa4b
Alexander Peters <businessonly1011@gmail.com>
2020-02-19 13:39:38 -0500

    Removed feeder limit switches, code in branch

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,51 +1,28 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.HardwareAdapter;
 
 public class Feeder extends SubsystemBase implements HardwareAdapter {
-  int numBalls = 0;
-
-  boolean[] lastSwitchStates = new boolean[2]; //keeps track of the last state of the limit switches
   public Feeder() {
-    numBalls = 0;
-    updateSwitchStates();
   }
 
   public void feedShooter() {
-    feeder.set(1);
-    feeder2.set(1);
+    ballFeeder.set(1);
+    shooterFeeder.set(1);
   }
 
   public void pushUpFeeder() {
-    feeder.set(1);
-    feeder2.set(-.5);
+    ballFeeder.set(1);
+    shooterFeeder.set(-.5);
   }
 
   public void stopFeed() {
-    feeder.set(0);
-    feeder2.set(0);
+    ballFeeder.set(0);
+    shooterFeeder.set(0);
   }
 
-  private void updateSwitchStates() {
-    lastSwitchStates[0] = chamberOut.get();
-    lastSwitchStates[1] = chamberIn.get();
-  }
-
-  // tracks number of balls
   @Override
   public void periodic() {
-    if(lastSwitchStates[0] == false && chamberOut.get() == true) {
-      numBalls--;
-    }
-
-    if(lastSwitchStates[1] == false && chamberIn.get() == true) {
-      numBalls++;
-    }
-
-    SmartDashboard.putNumber("Balls", 0);
-
-    updateSwitchStates();
   }
 }
