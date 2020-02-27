76cdefeb5a6bac3d8bf30c69b9113b8221262a9d
Rulan <rulangu@gmail.com>
2020-02-27 10:58:03 -0500

    added encoder methods for turret/hood

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -9,20 +9,45 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.HardwareAdapter;
 
 public class Hood extends SubsystemBase implements HardwareAdapter {
+
+  NetworkTable table;
+  NetworkTableEntry pos, velo;
+
   public Hood() {
     hoodMotor.setNeutralMode(NeutralMode.Brake);
+
+    table = NetworkTableInstance.getDefault().getTable("Hood");
+    pos = table.getEntry("Hood Position Ticks");
+    velo = table.getEntry("Hood Velocity Ticks");
   }
 
   public void drive(double throttle) {
     hoodMotor.set(throttle*.5);
   }
 
+  public int getPositionTicks() { // 4096 ticks per rot
+    return hoodMotor.getSelectedSensorPosition();
+  }
+
+  public int getVelocityTicks() {
+    return hoodMotor.getSelectedSensorVelocity();
+  }
+
+  public void resetEncoder() {
+    hoodMotor.setSelectedSensorPosition(0);
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    pos.setDouble(getPositionTicks());
+    velo.setDouble(getVelocityTicks());
+
   }
 }
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -9,20 +9,44 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.HardwareAdapter;
 
 public class Turret extends SubsystemBase implements HardwareAdapter {
+  NetworkTable table;
+  NetworkTableEntry pos, velo;
+
   public Turret() {
     turretMotor.setNeutralMode(NeutralMode.Brake);
+    
+    table = NetworkTableInstance.getDefault().getTable("Turret");
+    pos = table.getEntry("Turret Position Ticks");
+    velo = table.getEntry("Turret Velocity Ticks");
   }
 
   public void drive(double throttle) {
     turretMotor.set(throttle*.5);
   }
 
+  public int getPositionTicks() { // 4096 ticks per rot
+    return turretMotor.getSelectedSensorPosition();
+  }
+
+  public int getVelocityTicks() {
+    return turretMotor.getSelectedSensorVelocity();
+  }
+
+  public void resetEncoder() {
+    turretMotor.setSelectedSensorPosition(0);
+  }
+
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    pos.setDouble(getPositionTicks());
+    velo.setDouble(getVelocityTicks());
   }
+
 }
